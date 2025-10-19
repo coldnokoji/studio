bd0284748dc9ba6df48bb4d3990870d271c506c6
Aryan Mishra <mishraaryan26aryan@gmail.com>
2025-10-19 15:18:46 +0000

    i have replcaed the logo with new one can you atleast  tale care of the

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,13 +6,13 @@ export function Logo() {
   return (
     <Link href="/" aria-label="Shreyaskar Social Welfare Foundation Home">
       <div className="flex items-center gap-3 pl-2">
-        <Image src={logoImage} alt="Shreyaskar Social Welfare Foundation Logo" width={48} height={48} />
-        <div className="flex flex-col text-left">
+        <Image src={logoImage} alt="Shreyaskar Social Welfare Foundation Logo" width={250} height={60} />
+        {/* <div className="flex flex-col text-left">
           <span className="text-lg font-bold text-foreground font-headline leading-tight">
             Shreyaskar
           </span>
           <span className="text-xs text-foreground/80 leading-tight">Social Welfare Foundation</span>
-        </div>
+        </div> */}
       </div>
     </Link>
   );
