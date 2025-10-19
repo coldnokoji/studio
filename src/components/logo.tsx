845022497b673c48452b4483980579c8222fb148
Aryan Mishra <mishraaryan26aryan@gmail.com>
2025-10-19 15:27:01 +0000

    logo changed

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
   return (
     <Link href="/" aria-label="Shreyaskar Social Welfare Foundation Home">
       <div className="flex items-center gap-3 pl-2">
-        <Image src={logoImage} alt="Shreyaskar Social Welfare Foundation Logo" width={300} height={50} />
+        <Image src={logoImage} alt="Shreyaskar Social Welfare Foundation Logo" width={280} />
         {/* <div className="flex flex-col text-left">
           <span className="text-lg font-bold text-foreground font-headline leading-tight">
             Shreyaskar
